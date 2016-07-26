bb3c8434554120e618b5668e15fa1f4536f5b6ff
guaty <bjarocha911@gmail.com>
2016-07-26 17:46:55 +0000

    Fix config typos

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,7 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
   
-  config.action_mailer.delivery_options = :smtp
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = { host: 'guaty-saas.herokuapp.com', protocol: 'https' }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
